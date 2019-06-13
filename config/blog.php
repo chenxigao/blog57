3fd356869ddee1ebc65a55ed21c3c28d7d76dee8
vagrant <vagrant@homestead>
2019-06-13 09:23:28 +0000

    后台实现文件上传删除管理功能

--- config/blog.php
+++ config/blog.php
@@ -2,4 +2,8 @@
 return [
         'title'          => 'Spring',
         'posts_per_page' => 5,
+        'uploads'        => [
+                'storage' => 'public',
+                'webpath' => '/storage',
+        ],
 ];
